2b34a02f618457807efa829cb0bb10b4d1deda54
Maikon Guimaraes <andersonmaikon93@gmail.com>
2025-07-28 10:09:48 -0300

    fix: add title in the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,6 +52,10 @@ export default function Home() {
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-grow px-4 py-8 max-w-7xl mx-auto w-full">
+          <h1 className="text-5xl font-bold text-gray-800 text-center mb-5">
+            Tudo sobre WordPress para Desenvolvedores Modernos
+          </h1>
+
           <div className="post-list grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
             {posts.map((post) => (
               <PostCard
